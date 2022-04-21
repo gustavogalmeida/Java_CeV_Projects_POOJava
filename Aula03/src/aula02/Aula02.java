af6237ae5fe653926649d837180cb97f412f59ad
Gustavo Almeida <gustavogalvesalmeida@gmail.com>
2022-04-21 17:36:13 -0300

    Aula de visibilidade: private, public & protected
    Aula de visibilidade: private, public & protected

---
+++ Aula03/src/aula02/Aula02.java
@@ -0,0 +1,13 @@
+package aula02;
+
+public class Aula02 {
+    public static void main(String[] args) {
+        // por padrão os atributos e metodos são publicos para pacote        
+        Caneta c1 = new Caneta(); // instanciando um objeto
+        c1.modelo = "bic cristal";
+        // c1.ponta = 0.5f; ponta é privado
+        c1.status(); // chamando um metodo 
+        
+    }
+    
+}
---
+++ Aula03/src/aula02/Caneta.java
@@ -0,0 +1,32 @@
+package aula02;
+public class Caneta {
+    public String cor;
+    public String modelo;
+    private float ponta;
+    protected int carga;
+    protected boolean tampada;
+    
+    public void rabiscar (){
+        if (this.tampada == true){
+            System.out.println("Nao e possuivel rabiscar, caneta tamapada.");
+        } else {
+            System.out.println("Rabiscando...");
+        }
+    }
+    
+    protected void tampar(){
+        this.tampada=true;
+    }
+    
+    protected void destampar(){
+        this.tampada=false;
+    }
+    
+    public void status(){
+        System.out.println("Uma caneta "+ this.cor+".");
+        System.out.println("Esta tampada? "+this.tampada+".");
+        System.out.println("A ponta e: "+ this.ponta+".");
+        System.out.println("O modelo e: "+ this.modelo+".");
+        System.out.println("A carga atual e: "+ this.carga+".");
+    }          
+}
