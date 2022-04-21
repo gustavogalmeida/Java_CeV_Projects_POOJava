0ea24af1656ecee92c0f74e32e6d6c11d875cd57
Gustavo Almeida <gustavogalvesalmeida@gmail.com>
2022-04-21 13:02:59 -0300

    Inciando com classe, metodos e objetos

--- Aula02/src/aula02/Aula02.java
+++ Aula02/src/aula02/Aula02.java
@@ -5,9 +5,9 @@ public class Aula02 {
         Caneta c1 = new Caneta(); // instanciando um objeto
         c1.cor="Azul"; // mudando atributo do objeto c1
         c1.ponta = 0.5f; // mudando atributo do objeto c1
-        c1.tampada=false; // mudando atributo do objeto c1
+        c1.tampada=true; // mudando atributo do objeto c1
         c1.status(); // chamando um metodo
-        c1.tampar();
+        c1.destampar();
         c1.rabiscar();
     }
     
