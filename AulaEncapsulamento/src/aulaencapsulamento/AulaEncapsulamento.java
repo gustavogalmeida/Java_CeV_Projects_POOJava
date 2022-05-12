bca845426af1d5a70c59a22142b26cc2488bcc2e
Gustavo Almeida <gustavogalvesalmeida@gmail.com>
2022-05-12 13:06:52 -0300

    Criado meus metodos do implements e colocado instruções

--- AulaEncapsulamento/src/aulaencapsulamento/AulaEncapsulamento.java
+++ AulaEncapsulamento/src/aulaencapsulamento/AulaEncapsulamento.java
@@ -3,6 +3,9 @@ package aulaencapsulamento;
 public class AulaEncapsulamento {
 
     public static void main(String[] args) {
-        
+        ControleRemoto c = new ControleRemoto();
+        c.ligar();
+        c.maisVolume();
+        c.abrirMenu();
     }
 }
