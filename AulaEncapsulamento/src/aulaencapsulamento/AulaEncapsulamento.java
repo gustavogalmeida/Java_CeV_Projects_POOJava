09f734936ee85e9d0fc61651326266b064fd8937
Gustavo Almeida <gustavogalvesalmeida@gmail.com>
2022-05-12 12:31:26 -0300

    Criado classe controle remoto, getters, setters e construct
    Criado classe controle remoto, getters, setters e construct

--- AulaEncapsulamento/src/aulaencapsulamento/AulaEncapsulamento.java
+++ AulaEncapsulamento/src/aulaencapsulamento/AulaEncapsulamento.java
@@ -5,5 +5,4 @@ public class AulaEncapsulamento {
     public static void main(String[] args) {
         
     }
-    
 }
---
+++ AulaEncapsulamento/src/aulaencapsulamento/ControleRemoto.java
@@ -0,0 +1,39 @@
+package aulaencapsulamento;
+
+public class ControleRemoto {
+    // atributos
+    private int volume;
+    private boolean ligado;
+    private boolean tocando;
+    // metodos especiais
+
+    public ControleRemoto() {
+        this.volume = 50;
+        this.ligado = false;
+        this.tocando = false;
+    }
+
+    public int getVolume() {
+        return volume;
+    }
+
+    public void setVolume(int volume) {
+        this.volume = volume;
+    }
+
+    public boolean isLigado() {
+        return ligado;
+    }
+
+    public void setLigado(boolean ligado) {
+        this.ligado = ligado;
+    }
+
+    public boolean isTocando() {
+        return tocando;
+    }
+
+    public void setTocando(boolean tocando) {
+        this.tocando = tocando;
+    }
+}
