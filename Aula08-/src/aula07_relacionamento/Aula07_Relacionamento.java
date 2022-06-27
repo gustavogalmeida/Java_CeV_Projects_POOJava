59649f26d0755c3d650083c41683000820a4fa33
Gustavo Almeida <gustavogalvesalmeida@gmail.com>
2022-06-27 16:31:12 -0300

    Iniciando relacionamento de classes e objetos abstratos com exercio 7

---
+++ Aula08-/src/aula07_relacionamento/Aula07_Relacionamento.java
@@ -0,0 +1,12 @@
+
+package aula07_relacionamento;
+
+public class Aula07_Relacionamento {
+
+    public static void main(String[] args) {
+        Lutador jonh = new Lutador("John", "America", 42, 1.75f, 85, 11, 3, 2);
+        jonh.apresentar();
+        jonh.status();
+    }
+    
+}
